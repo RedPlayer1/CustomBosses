5d60d08fd460577e281500f54b31d9f4bc0743c4
BuildTools <unconfigured@null.spigotmc.org>
2023-12-24 17:28:38 -0500

    add ability for console to spawn Bosses

--- src/main/java/me/redplayer_1/custombosses/command/BossCommand.java
+++ src/main/java/me/redplayer_1/custombosses/command/BossCommand.java
@@ -2,6 +2,9 @@ package me.redplayer_1.custombosses.command;
 
 import me.redplayer_1.custombosses.boss.Boss;
 import me.redplayer_1.custombosses.boss.BossType;
+import me.redplayer_1.custombosses.util.MessageUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -17,34 +20,50 @@ public class BossCommand extends Command {
 
     @Override
     public boolean execute(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] args) {
-        if (!(sender instanceof Player player)) return true;
+        if (args.length < 2) return false;
+        if (args[1].equalsIgnoreCase("despawn")) {
+            // despawn all bosses of that type
+            String targetBoss = BossType.valueOf(args[0].toUpperCase()).name();
+            List<Boss> despawnQueue = new LinkedList<>();
 
-        try {
-            if (args[1].equalsIgnoreCase("despawn")) {
-                // despawn all bosses of that type
-                String targetBoss = BossType.valueOf(args[0].toUpperCase()).name();
-                List<Boss> despawnQueue = new LinkedList<>();
-
-                for (Map.Entry<UUID, Boss> i : Boss.getRegistry().entrySet()) {
-                    if (targetBoss.equalsIgnoreCase(args[0])) {
-                        despawnQueue.add(i.getValue());
-                    }
-                }
-                for (Boss i : despawnQueue) {
-                    i.despawn();
-                }
-            } else if (args[1].equalsIgnoreCase("spawn")) {
-                // spawn a new boss of that type
-                try {
-                    Boss boss = BossType.valueOf(args[0].toUpperCase()).newInstance();
-                    player.sendPlainMessage("&bSpawning a " + boss.getConfig().name() + ".");
-                    boss.spawn(player.getLocation(), player);
-                } catch (IllegalArgumentException e) {
-                    player.sendPlainMessage("Invalid Name!");
+            for (Map.Entry<UUID, Boss> i : Boss.getRegistry().entrySet()) {
+                if (targetBoss.equalsIgnoreCase(args[0])) {
+                    despawnQueue.add(i.getValue());
                 }
             }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            player.sendPlainMessage("Invalid Arguments!");
+            for (Boss i : despawnQueue) {
+                i.despawn();
+            }
+        }
+        if (sender instanceof Player player && args[1].equalsIgnoreCase("spawn")) {
+            // spawn a new boss of that type
+            try {
+                Boss boss = BossType.valueOf(args[0].toUpperCase()).newInstance();
+                player.sendMessage(MessageUtils.mmsgToComponent("<gray>Spawning a <red><i>" + boss.getConfig().name() + "."));
+                boss.spawn(player.getLocation(), player);
+            } catch (IllegalArgumentException e) {
+                player.sendPlainMessage("Invalid Name!");
+                return false;
+            }
+        } else {
+            // coords must be specified if used from console (arg 2-5)
+            // x, y, z, world
+            if (args.length < 6) return false;
+            int[] coords = new int[]{
+                    Integer.parseInt(args[2]),
+                    Integer.parseInt(args[3]),
+                    Integer.parseInt(args[4])
+            };
+            if (args[1].equalsIgnoreCase("spawn")) {
+                try {
+                    BossType.valueOf(args[0].toUpperCase()).newInstance().spawn(
+                            new Location(Bukkit.getWorld(args[5]), coords[0], coords[1], coords[2]),
+                            null
+                    );
+                } catch (NullPointerException | NumberFormatException ignored) {
+                    return false;
+                }
+            }
         }
         return true;
     }
