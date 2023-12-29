2528fc2936ab52fc46f4293a76dc0300801ba48f
BuildTools <unconfigured@null.spigotmc.org>
2023-12-29 11:01:51 -0500

    add StasisAbility & MinerZombie

---
+++ src/main/java/me/redplayer_1/custombosses/abilities/impl/StasisAbility.java
@@ -0,0 +1,41 @@
+package me.redplayer_1.custombosses.abilities.impl;
+
+import me.redplayer_1.custombosses.abilities.BossAbility;
+import me.redplayer_1.custombosses.abilities.CooldownBossAbility;
+import me.redplayer_1.custombosses.boss.Boss;
+import me.redplayer_1.custombosses.util.CachedList;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.concurrent.TimeUnit;
+
+public class StasisAbility extends CooldownBossAbility {
+    private static final int DURATION = 4;
+    private static final CachedList<Player> cache = new CachedList<>(DURATION, TimeUnit.SECONDS);
+
+    public StasisAbility() {
+        this(0.8);
+    }
+    public StasisAbility(double chance) {
+        super("<gradient:green:yellow>Stasis</gradient>", false, chance, DURATION * 20);
+    }
+
+    @Override
+    public BossAbility newInstance() {
+        return new StasisAbility();
+    }
+
+    @Override
+    public boolean use(Boss boss, Player target) {
+        target.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, DURATION * 20, 69, false, true, false));
+        cache.add(target);
+        // TODO: add particle (helix/sphere around affected players)
+        return true;
+    }
+
+    @SuppressWarnings("deprecation")
+    public static boolean handleMove(Player p) {
+        return !p.isOnGround() && cache.contains(p);
+    }
+}
---
+++ src/main/java/me/redplayer_1/custombosses/boss/impl/MinerZombie.java
@@ -0,0 +1,64 @@
+package me.redplayer_1.custombosses.boss.impl;
+
+import me.redplayer_1.custombosses.abilities.impl.InvincibilityAbility;
+import me.redplayer_1.custombosses.abilities.impl.MinionAbility;
+import me.redplayer_1.custombosses.abilities.impl.RegenAbility;
+import me.redplayer_1.custombosses.abilities.impl.StasisAbility;
+import me.redplayer_1.custombosses.boss.Boss;
+import me.redplayer_1.custombosses.boss.BossType;
+import me.redplayer_1.custombosses.config.providers.BossConfig;
+import me.redplayer_1.custombosses.util.ItemUtils;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class MinerZombie extends Boss {
+    private static final ItemStack TROPHY = ItemUtils.createItem(
+            Material.SEA_LANTERN,
+            "<dark_green>Miner Zombie Trophy</dark_green>",
+            List.of("<gray>Obtained from killing a <dark_green><i>Miner Zombie</i></dark_green>")
+    );
+
+    public MinerZombie() {
+        super(new BossConfig(
+                "ZOMBIE", BossType.MINER_ZOMBIE, "Miner Zombie", 150, 20),
+                1,
+                new RegenAbility(0.4),
+                new StasisAbility(0.9),
+                new InvincibilityAbility(0.7),
+                new MinionAbility(0.89)
+        );
+    }
+
+    @Override
+    public void onPreSpawn(Location spawnLocation, SpawnBuilder builder) {
+
+    }
+
+    @Override
+    public void onSpawn() {
+        EntityEquipment equipment = getMob().getEntity().getEquipment();
+        equipment.setHelmet(new ItemStack(Material.SEA_LANTERN));
+        equipment.setHelmetDropChance(0);
+        equipment.setChestplate(new ItemStack(Material.CHAINMAIL_CHESTPLATE));
+        equipment.setChestplateDropChance(0);
+        equipment.setLeggings(new ItemStack(Material.CHAINMAIL_LEGGINGS));
+        equipment.setLeggingsDropChance(0);
+        equipment.setBoots(new ItemStack(Material.LEATHER_BOOTS));
+        equipment.setBootsDropChance(0);
+
+        equipment.setItemInMainHand(new ItemStack(Material.NETHERITE_PICKAXE));
+        equipment.setItemInMainHandDropChance(0);
+    }
+
+    @Override
+    public void onKill(@NotNull Location location, @Nullable LivingEntity killer) {
+        ItemUtils.giveOrDrop(killer, location, TROPHY);
+    }
+}
