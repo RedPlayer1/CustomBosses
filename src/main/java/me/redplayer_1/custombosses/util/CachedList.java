d123746074a19c6a287fbc603d29e5c70ead6d5e
BuildTools <unconfigured@null.spigotmc.org>
2023-12-29 10:10:55 -0500

    add removeAction

--- src/main/java/me/redplayer_1/custombosses/util/CachedList.java
+++ src/main/java/me/redplayer_1/custombosses/util/CachedList.java
@@ -8,14 +8,30 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 public class CachedList<T> implements Iterable<T>{
+    private record CacheEntry<E>(E item, long insertTime) {}
+
     private final LinkedList<CacheEntry<T>> list = new LinkedList<>();
     private final ScheduledTask clearTask;
+    private Consumer<T> removeAction = null;
 
+    /**
+     * @param lifetime the lifetime of elements added to the list
+     * @param unit the unit of time the lifetime represents
+     */
     public CachedList(int lifetime, TimeUnit unit) {
         clearTask = Bukkit.getAsyncScheduler().runAtFixedRate(CustomBosses.getInstance(),
-                task -> list.removeIf(entry -> System.currentTimeMillis() - entry.insertTime >= unit.toMillis(lifetime)),
+                task -> {
+                    for (int i = 0; i < list.size(); i++) {
+                        if (System.currentTimeMillis() - list.get(i).insertTime >= unit.toMillis(lifetime)) {
+                            if (removeAction != null) removeAction.accept(list.get(i).item);
+                            list.remove(i);
+                            i--;
+                        }
+                    }
+                },
                 lifetime, lifetime, unit
         );
     }
@@ -26,6 +42,14 @@ public class CachedList<T> implements Iterable<T>{
         for (T item : contents) {
             add(item);
         }
+    }
+
+    /**
+     * @param removeAction called before an element is removed from the list
+     */
+    public CachedList(int lifetime, TimeUnit unit, Consumer<T> removeAction) {
+        this(lifetime, unit);
+        this.removeAction = removeAction;
     }
 
     public void add(T e) {
@@ -77,6 +101,4 @@ public class CachedList<T> implements Iterable<T>{
             }
         };
     }
-
-    public record CacheEntry<E>(E item, long insertTime) {}
 }
