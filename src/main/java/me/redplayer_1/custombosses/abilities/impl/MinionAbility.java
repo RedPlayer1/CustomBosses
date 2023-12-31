a0dbb3b427305de35242514a666fc3bd2f3399db
BuildTools <unconfigured@null.spigotmc.org>
2023-12-30 20:42:46 -0500

    fix MinionAbility

--- src/main/java/me/redplayer_1/custombosses/abilities/impl/MinionAbility.java
+++ src/main/java/me/redplayer_1/custombosses/abilities/impl/MinionAbility.java
@@ -1,17 +1,33 @@
 package me.redplayer_1.custombosses.abilities.impl;
 
-import me.redplayer_1.custombosses.abilities.BossAbility;
+import me.redplayer_1.custombosses.CustomBosses;
 import me.redplayer_1.custombosses.abilities.CooldownBossAbility;
 import me.redplayer_1.custombosses.boss.Boss;
+import me.redplayer_1.custombosses.boss.BossDeathEvent;
 import me.redplayer_1.custombosses.util.MessageUtils;
 import org.bukkit.Location;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+
 public class MinionAbility extends CooldownBossAbility {
+    private static final int AMOUNT = 5;
+    private static final NamespacedKey KEY = new NamespacedKey(CustomBosses.getInstance(), "minion_owner_id");
+    private static HashMap<UUID, HashMap<Integer, LivingEntity>> minions = new HashMap<>();
+
     public MinionAbility() {
         this(0.4);
     }
@@ -21,22 +37,44 @@ public class MinionAbility extends CooldownBossAbility {
     }
 
     @Override
-    public BossAbility newInstance() {
-        return new MinionAbility();
-    }
-
-    @Override
     public boolean use(Boss boss, Player target) {
         Location loc = boss.getLocation();
-        if (loc == null) return false;
 
-        for (int i = 0; i < 5; i++) {
-            // give the minions fire resistance, so they don't burn during the day
-            LivingEntity entity = (LivingEntity) loc.getWorld().spawnEntity(loc, EntityType.ZOMBIE);
-            entity.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, PotionEffect.INFINITE_DURATION, 1, false, false));
-            entity.customName(MessageUtils.mmsgToComponent(getName()));
-            entity.setCustomNameVisible(true);
+        if (loc == null) return false;
+        HashMap<Integer, LivingEntity> localMinions = minions.get(boss.getMob().getUuid());
+        if (localMinions == null) localMinions = new HashMap<>();
+
+        for (int index = 0; index < AMOUNT; index++) {
+            if (!localMinions.containsKey(index)) {
+                LivingEntity entity = (LivingEntity) loc.getWorld().spawnEntity(loc, EntityType.ZOMBIE);
+                // give the minions fire resistance, so they don't burn during the day
+                entity.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, PotionEffect.INFINITE_DURATION, 1, false, false));
+                entity.customName(MessageUtils.mmsgToComponent(getName()));
+                entity.setCustomNameVisible(true);
+                entity.getPersistentDataContainer().set(KEY, PersistentDataType.STRING, boss.getMob().getUuid().toString() + ":" + index);
+                localMinions.put(index, entity);
+            }
         }
+
+        minions.put(boss.getMob().getUuid(), localMinions);
         return true;
+    }
+
+    public static class MinionListener implements Listener {
+        @EventHandler
+        public void onMinionDeath(EntityDeathEvent event) {
+            String uuid = event.getEntity().getPersistentDataContainer().get(KEY, PersistentDataType.STRING);
+            if (uuid != null) {
+                String[] data = uuid.split(":");
+                minions.get(UUID.fromString(data[0])).remove(Integer.parseInt(data[1]));
+            }
+        }
+
+        @EventHandler
+        public void onBossDeath(BossDeathEvent event) {
+            HashMap<Integer, LivingEntity> targetMinions = minions.get(event.getKilled().getMob().getUuid());
+            if (targetMinions != null)
+                targetMinions.forEach((index, entity) -> entity.remove());
+        }
     }
 }
