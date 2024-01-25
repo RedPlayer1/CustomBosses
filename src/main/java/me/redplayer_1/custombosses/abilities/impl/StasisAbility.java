4ad5a9a34b159eeaf946bd8ecc3b77f197bd3a65
BuildTools <unconfigured@null.spigotmc.org>
2024-01-25 18:37:15 -0500

    make StasisAbility look better

--- src/main/java/me/redplayer_1/custombosses/abilities/impl/StasisAbility.java
+++ src/main/java/me/redplayer_1/custombosses/abilities/impl/StasisAbility.java
@@ -3,6 +3,8 @@ package me.redplayer_1.custombosses.abilities.impl;
 import me.redplayer_1.custombosses.abilities.CooldownBossAbility;
 import me.redplayer_1.custombosses.boss.BossEntity;
 import me.redplayer_1.custombosses.util.CachedList;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,7 +17,11 @@ import java.util.concurrent.TimeUnit;
 
 public class StasisAbility extends CooldownBossAbility {
     private static final int DURATION = 4;
-    private static final CachedList<Player> cache = new CachedList<>(DURATION, TimeUnit.SECONDS);
+    private static final CachedList<Player> cache = new CachedList<>(DURATION, TimeUnit.SECONDS,
+            player -> {
+                AttributeInstance attr = player.getAttribute(Attribute.GENERIC_KNOCKBACK_RESISTANCE);
+                attr.setBaseValue(attr.getBaseValue() - 1);
+            });
 
     public StasisAbility() {
         this(0.8);
@@ -27,6 +33,8 @@ public class StasisAbility extends CooldownBossAbility {
     @Override
     public boolean use(BossEntity bossEntity, Player target) {
         target.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, DURATION * 20, 69, false, true, false));
+        AttributeInstance attr = target.getAttribute(Attribute.GENERIC_KNOCKBACK_RESISTANCE);
+        attr.setBaseValue(attr.getBaseValue() + 1); // don't take kb from attacks (no rubberbanding)
         cache.add(target);
         // TODO: add particle (helix/sphere around affected players)
         return true;
@@ -35,7 +43,11 @@ public class StasisAbility extends CooldownBossAbility {
     public static class StasisListener implements Listener {
         @EventHandler
         public void onPlayerMove(PlayerMoveEvent event) {
-            event.setCancelled(event.getTo().getY() > event.getFrom().getY() && cache.contains(event.getPlayer()));
+            if (event.getTo().getY() > event.getFrom().getY() && cache.contains(event.getPlayer())) {
+                event.setCancelled(true);
+                event.getPlayer().setFreezeTicks(120);
+            }
+
         }
 
         @EventHandler
